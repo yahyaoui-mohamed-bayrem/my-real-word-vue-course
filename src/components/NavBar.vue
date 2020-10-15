be77066348640cc0ed1ed5073fad7898e98cb907
Badran <mohamedbadran.yahyaoui@esprit.tn>
2020-10-15 15:19:58 +0100

    9 Lessons completed

---
+++ src/components/NavBar.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="nav" class="nav">
+    <router-link to="/" class="brand">Real World Events</router-link> |
+    <nav>
+      <router-link :to="{ name: 'event-list' }">List</router-link> |
+      <router-link :to="{ name: 'event-create' }">Create page</router-link> |
+      <router-link :to="{ name: 'dr01', params: { username01: 'expl01' } }">
+        dinamic routs 01
+      </router-link>
+      |
+      <router-link :to="{ name: 'dr02', params: { username02: 'expl02' } }">
+        dinamic routs 02
+      </router-link>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 60px;
+}
+.nav > .brand {
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 700;
+  font-size: 1.5em;
+  color: #39b982;
+  text-decoration: none;
+}
+.nav .nav-item {
+  box-sizing: border-box;
+  margin: 0 5px;
+  color: rgba(0, 0, 0, 0.5);
+  text-decoration: none;
+}
+.nav .nav-item.router-link-exact-active {
+  color: #39b982;
+  border-bottom: solid 2px #39b982;
+}
+</style>
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,15 +1,33 @@
 <template>
   <div>
-    <h1>Events List</h1>
-    <router-link :to="{ name: 'event-show', params: { id: '1' } }">
-      Show Event #1
-    </router-link>
+    <h1>Events Listing</h1>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <BaseIcon />
   </div>
 </template>
 
 <script>
+import EventCard from '@/components/EventCard.vue'
+import EventService from '@/services/EventService.js'
 export default {
-  name: 'EventList'
+  name: 'EventList',
+  components: {
+    EventCard
+  },
+  data() {
+    return {
+      events: []
+    }
+  },
+  created() {
+    EventService.getEvents()
+      .then(response => {
+        this.events = response.data
+      })
+      .catch(error => {
+        console.log('There was an error:' + error.response)
+      })
+  }
 }
 </script>
 
