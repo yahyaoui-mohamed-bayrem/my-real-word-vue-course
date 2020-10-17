0dd4857776638de048037d7494035c890a3f3e08
Mohamed Bayrem YAHYAOUI <mb.yahyaoui@SCANTECH.local>
2020-10-17 12:19:07 +0100

    Mastering Vuex course: lessons 1, 2, 3 and 4 (Mutations & Actions Part 1) completed

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,50 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: {
+      id: 'abc123', 
+      name: 'adam'
+    },
+    categories: [
+      'sustanability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education', 
+      'food', 
+      'community'
+    ],
+    todos: [
+      { id: 1, text: '...', done: true },
+      { id: 2, text: '...', done: false },
+      { id: 3, text: '...', done: true },
+      { id: 4, text: '...', done: false},
+    ],
+    events: [
+      { id: 1, title: '...', organizer: '...' },
+      { id: 2, title: '...', organizer: '...' },
+      { id: 3, title: '...', organizer: '...' },
+      { id: 4, title: '...', organizer: '...' },
+    ]
+  },
   mutations: {},
   actions: {},
-  modules: {}
+  modules: {},
+  getters: {
+    catLength: state => {// in case we want to use categ length in multiple places
+      return state.categories.length
+    },
+
+    // using a getter inside a getter
+  //   doneTodos: state => {
+  //     return state.todos.filter(todo => todo.done)
+  //   },
+  //   activeTodosCount: (state, getters) => state.todo.length - getters.doneTodos.length
+  
+  activeTodosCount: state => state.todos.filter(todo => !todo.done).length,
+
+  //dinamic getters => with an argument
+    getEventByID: state => id => state.events.find(event => event.id === id)
+}
 });
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,13 +1,110 @@
 <template>
   <div>
-    <h1>Create event</h1>
+    <!-- Mastering Vuex course lesson 4 : mutation & actions -->
+    <h1>Create an event</h1>
+  <form @submit.prevent="createEvent">
+      <label>Select a category</label>
+      <select v-model="event.category">
+        <option v-for="cat in categories" :key="cat">{{ cat }}</option>
+      </select>
+
+      <h3>Name & describe your event</h3>
+      <div class="field">
+        <label>Title</label>
+        <input v-model="event.title" type="text" placeholder="Add an event title"/>
+      </div>
+
+      <div class="field">
+        <label>Description</label>
+        <input v-model="event.description" type="text" placeholder="Add a description"/>
+      </div>
+
+      <h3>Where is your event?</h3>
+      <div class="field">
+        <label>Location</label>
+        <input v-model="event.location" type="text" placeholder="Add a location"/>
+      </div>
+
+      <h3>When is your event?</h3>
+
+      <div class="field">
+        <label>Date</label>
+        <datepicker v-model="event.date" placeholder="Select a date"/>
+      </div>
+
+      <div class="field">
+        <label>Select a time</label>
+        <select v-model="event.time">
+
+          <option v-for="time in times" :key="time">{{ time }}</option>
+
+        </select>
+      </div>
+
+      <input type="submit" class="button -fill-gradient" value="Submit"/>
+
+    </form>
+
+    <!-- Mastering Vuex course lesson 3 : state & getters -->
+    <!-- <h1>Create event, {{ user.name }}</h1>
+    <p> this event was created by {{ user.id }}</p>
+    <p>{{ getEventByID(2) }}</p>
+    <p>there are {{ catLength }} categories :</p>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul> -->
   </div>
 </template>
 
 <script>
+// Mastering Vuex course lesson 4 : mutation & actions
+import Datepicker from 'vuejs-datepivcker'
+
 export default {
-  name: 'EventCreate'
+  components: {
+    Datepicker
+  },
 }
+
+//  Mastering Vuex course lesson 3 : state & getters :
+// import { mapState, mapGetters } from 'vuex'
+
+// export default {
+//   name: 'EventCreate',
+//   // this way we can name / rename our computed props
+//   // computed: mapState({
+//   //   //userName: state => state.user.name,
+//   //   //userID: state => state.user.id,
+//   //   user: 'user',
+//   //   categories: 'categories'
+//   // })
+
+//   //if we don't need to name our computed prop we can use this shortcut
+//   computed: {
+//     ...mapState(['user', 'categories']),
+//     autherComputedProps() {
+//       return null
+//     },
+//     catLength() {
+//       return this.$store.getters.catLength
+//     },    
+
+//   //this computed prop declaration is without mapState
+// //   computed: {
+// //     userName() {
+// //       return this.$store.state.user.name
+// //   },
+// //   userID() {
+// //       return this.$store.state.user.id
+// //   }
+    
+//     // getEvent() {
+//     //   return this.$store.getters.getEventByID
+//     // }
+//     // or
+//     ...mapGetters(['getEventByID'])
+//  }
+// }
 </script>
 
 <style></style>
